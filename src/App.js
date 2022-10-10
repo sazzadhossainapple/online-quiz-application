ed3737a2f1f09cd95924d4aa72fea1e1e419901b
sazzadhossainapple <sazzadapple81@gmail.com>
2022-10-11 05:25:28 +0600

    correct answer add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import { router } from "./components/Routes/Routes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="">
+      <ToastContainer position="top-center" />
       <RouterProvider router={router}></RouterProvider>
     </div>
   );
--- src/components/SingleQuiz/SingleQuiz.js
+++ src/components/SingleQuiz/SingleQuiz.js
@@ -1,21 +1,32 @@
 import { EyeIcon } from "@heroicons/react/24/solid";
-import React from "react";
+import { toast } from "react-toastify";
 import QuizQuestion from "../QuizQuestion/QuizQuestion";
 
 const SingleQuiz = ({ singleQuizQuestion }) => {
-  const { question, options } = singleQuizQuestion;
+  const { question, options, correctAnswer } = singleQuizQuestion;
+
+  const correctHandler = () => {
+    toast.success(`Answer : ${correctAnswer}`);
+  };
 
   return (
     <div className="shadow-lg p-9 mb-9">
       <div>
         <div className="flex justify-end mb-6">
-          <EyeIcon className="h-6 w-6 text-[#209CEE] " />
+          <EyeIcon
+            onClick={correctHandler}
+            className="h-6 w-6 text-[#209CEE] cursor-pointer "
+          />
         </div>
         <h2 className=" text-2xl font-medium  mb-6 ">{question}</h2>
       </div>
       <div className="grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 grid-cols-1 gap-5">
         {options.map((question, index) => (
-          <QuizQuestion key={index} question={question} />
+          <QuizQuestion
+            key={index}
+            question={question}
+            correctAnswer={correctAnswer}
+          />
         ))}
       </div>
     </div>
